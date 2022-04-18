a987ad05a23cf223f8b91a11c2e40ec7199a6a6c
kai-bunshow <awwwsuuuus@ooth.chitsu>
2022-04-18 21:08:17 +0900

    ブログ開始
    とりあえず上げるぜ！

---
+++ component---src-pages-404-jsx-b3ecffbe3438f09c08f3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkminimal_blog=self.webpackChunkminimal_blog||[]).push([[256],{37291:function(e,l,n){n.r(l);var t=n(67294),u=n(69748);l.default=function(){return t.createElement(u.Z,null,t.createElement("h1",null,"404 - Page Not Found"),t.createElement("p",null,"ざけんなよ！ そんなページなんか用意してねーよ！ :("))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-b3ecffbe3438f09c08f3.js.map
